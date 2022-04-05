0c8bd728a605d219ed24ca833f3b5eb85efd6abb
Lucas <teppss@gmail.com>
2022-04-05 11:18:03 +0100

    new cc and bcc fields update gettext_lazy for django 4

--- EmailTemplate/admin.py
+++ EmailTemplate/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import EmailTemplate
+from EmailTemplate.models import EmailTemplate
 from django_summernote.admin import SummernoteModelAdminMixin
 
 
